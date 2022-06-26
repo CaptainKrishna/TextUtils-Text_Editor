2379d98be58b9d1dc387fa5f0b823491f8ba8299
Krishna Vishwakarma <krishvishwa888@gmail.com>
2022-06-26 20:16:36 +0530

    Learn Map Funtion

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+// import Header from './my_Component/Header';
 import './App.css';
+// import Main from './my_Component/Main';
+// import Page from './my_Component/page';
+// import Banner from './my_Component/Banner';
+// import card from './my_Component/card';
+import Practice from './my_Component/practice';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <Practice/>
+   {/* <Header/>
+   <Banner/>
+   <card/>
+   <Main/>
+   <Page/> */}
+
+   </>
   );
 }
 
---
+++ src/my_Component/practice.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const fruits = [
+    {
+        "id": 1,
+        "name": "Banana",
+        "price": 125,
+        "href": "www.google.com",
+    },
+    {
+        "id": 2,
+        "name": "Apple",
+        "price": 224,
+        "href": "www.youtube.com",
+
+    },
+    {
+        "id": 3,
+        "name": "Papaya",
+        "price": 125,
+        "href": "www.instagram.com",
+
+    }
+]
+
+export default function Practice() {
+    return (
+        <>
+            {fruits.map((f) => (
+                <div key={f.id} className="container mx-auto" >
+                    <a href={f.href}>
+                        <h2 className="text-3xl font-bold bg-yellow-300 ml-2">{f.name}</h2>
+                        <h3 >{f.price}</h3>
+                    </a>
+                </div>
+            ))}
+        </>
+    )
+}
