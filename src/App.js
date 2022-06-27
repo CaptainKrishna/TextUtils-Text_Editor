92a823167d5743a1956a12e4c053d7318ccf7bcd
Krishna Vishwakarma <krishvishwa888@gmail.com>
2022-06-27 20:06:20 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -2,21 +2,14 @@ import Header from './my_Component/Header';
 import './App.css';
 import Main from './my_Component/Main';
 import Page from './my_Component/page';
-import Banner from './my_Component/Banner';
-import Card from './my_Component/card';
-// import Practice from './my_Component/practice';
 
 function App() {
-  
+
   return (
    <>
-   {/* <Practice/> */}
    <Header/>
-   <Banner/>
-   <Card/>
    <Main/>
    <Page/>
-
    </>
   );
 }
--- src/my_Component/Main.js
+++ src/my_Component/Main.js
@@ -76,9 +76,9 @@ export default function Example() {
   return (
     <div className="bg-white">
       <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-        <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">Explore products</h2>
+        <h2 className="text-5xl font-extrabold  tracking-tight text-gray-900">New Products</h2>
         {/* product Component */}
-        <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+        <div className="mt-12 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
 
           {products.map((product) => (
             
