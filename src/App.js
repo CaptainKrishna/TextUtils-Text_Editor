b2c6a15f3ba784c2fad4bc2438c1056cb5afcce1
Krishna Vishwakarma <krishvishwa888@gmail.com>
2022-07-02 00:36:39 +0530

    ui

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
    <>
    <Header/>
-   <Todo title="Todo List"/>
+   <Todo title="Text Editor"/>
    {/* <Main/>
    <Page/> */}
    </>
--- src/my_Component/Todo.js
+++ src/my_Component/Todo.js
@@ -17,24 +17,32 @@ export default function Todo(props) {
     setText(newText);
   };
   const handleCopy = () => {
-    var text=document.getElementById("Mybox")
-    text.select()
-    navigator.clipboard.writeText(text.value)
+    var text = document.getElementById("Mybox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  };
+  const handleRmovespace = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
   };
 
   const [text, setText] = useState("This is text area");
   return (
     <div>
       <h1 className="titleHead">{props.title}</h1>
-      <textarea
-        name="Todo"
-        value={text}
-        onChange={handleonChange}
-        cols="50"
-        rows="5"
-        className="texta"
-        id="Mybox"
-      ></textarea>
+      
+        <textarea
+          name="Todo"
+          value={text}
+          onChange={handleonChange}
+          cols="50"
+          rows="5"
+          className="contain"
+          // className="texta"
+          id="Mybox"
+        ></textarea>
+      <div className="container">
+
       <button className="btn" onClick={handleupClick}>
         Uppercase
       </button>
@@ -47,13 +55,18 @@ export default function Todo(props) {
       <button className="btn" onClick={handleCopy}>
         Copy
       </button>
-        <div className="container mx-12 my-5">
+      <button className="btn" onClick={handleRmovespace}>
+        Remove Spaces
+      </button>
+      </div>
+      <div className="container mx-12 my-5">
         <h2 className="m-2 text-xl font-semibold">Your Text Summery</h2>
-        <p className="">{text.split(" ").length-1} word and {text.length} charactor</p>
+        <p className="">
+          {text.split(" ").length - 1} word and {text.length} charactor
+        </p>
         <h2 className="text-xl font-semibold mt-5">Preview</h2>
-        <p className="bg-gray-200 mx-auto" >{text}</p>
-
-        </div>
+        <p>{text}</p>
+      </div>
     </div>
   );
 }
