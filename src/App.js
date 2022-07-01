72710bc25abf17e71b2681ca6f3bb4d11b1fafbd
Krishna Vishwakarma <krishvishwa888@gmail.com>
2022-07-01 23:30:17 +0530

    state change

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import Header from './my_Component/Header';
 import './App.css';
-import Main from './my_Component/Main';
-import Page from './my_Component/page';
+// import Main from './my_Component/Main';
+// import Page from './my_Component/page';
+import Todo from './my_Component/Todo';
 
 function App() {
 
   return (
    <>
    <Header/>
-   <Main/>
-   <Page/>
+   <Todo title="Todo List"/>
+   {/* <Main/>
+   <Page/> */}
    </>
   );
 }
---
+++ src/my_Component/Todo.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { useState } from "react";
+export default function Todo(props) {
+  const handleonChange = (event) => {
+    setText(event.target.value);
+  };
+  const handleupClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+  const handleDownClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+  const handleDelete = () => {
+    let newText = "";
+    setText(newText);
+  };
+
+  const [text, setText] = useState("This is text area");
+  return (
+    <div>
+      <h1 className="titleHead">{props.title}</h1>
+      <textarea
+        name="Todo"
+        value={text}
+        onChange={handleonChange}
+        cols="50"
+        rows="5"
+        className="texta"
+      ></textarea>
+      <button className="btn" onClick={handleupClick}>
+        Uppercase
+      </button>
+      <button className="btn" onClick={handleDownClick}>
+        Lowercase
+      </button>
+      <button className="btn" onClick={handleDelete}>
+        Clear
+      </button>
+        <div className="container mx-12 my-5">
+        <h2 className="m-2 text-xl font-semibold">Your Text Summery</h2>
+        <p className="">{text.split(" ").length-1} word and {text.length} charactor</p>
+        <h2 className="text-xl font-semibold mt-5">Preview</h2>
+        <p className="bg-gray-200 mx-auto" >{text}</p>
+
+        </div>
+    </div>
+  );
+}
