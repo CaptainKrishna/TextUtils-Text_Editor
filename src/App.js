ddb32f38d5b833a6a009917fa97d828632b9b406
Krishna Vishwakarma <krishvishwa888@gmail.com>
2022-07-08 23:47:31 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import Header from "./my_Component/Header";
-import Footer from "./my_Component/Footer";
-import Main from "./my_Component/Main";
+// import Header from "./my_Component/Header";
+// import Footer from "./my_Component/Footer";
+// import Main from "./my_Component/Main";
+import Card1 from "./my_Component/Card1";
 
 // import Todo from "./my_Component/Todo";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import CardCom from "./my_Component/CardCom";
 function App() {
   return (
-    <Router>
-        <Header/>
-          <Routes>
-        
-       <Route path="/">
-        <Main/>
-       </Route>
-      </Routes>
-      <Footer/>
-      </Router>
-  
+    <>       
+  <Card1/>
+  </>
   );
 }
 
