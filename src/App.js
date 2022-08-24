4c2f457b317aad2b9c8d158fbd5251837c4b1eae
Krishna Vishwakarma <krishvishwa888@gmail.com>
2022-08-24 20:57:47 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-// import Header from "./my_Component/Header";
-// import Footer from "./my_Component/Footer";
-// import Main from "./my_Component/Main";
-import Card1 from "./my_Component/Card1";
 
-// import Todo from "./my_Component/Todo";
 import "./App.css";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import CardCom from "./my_Component/CardCom";
+import Todo from "./my_Component/Todo";
+
 function App() {
   return (
     <>       
-  <Card1/>
+  
+<Todo title="TEXTUTILS"/>
   </>
   );
 }
